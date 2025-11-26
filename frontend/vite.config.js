172824f8f32e72de0761dfd7f37bd15990a9212d
Rj Bangcaya <rjbangcaya0@gmail.com>
2025-11-26 13:49:32 +0800

    Initial commit with Vite + React setup for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/payrollsystem/', // add this line
   server: {
     port: 3000,
     proxy: {
